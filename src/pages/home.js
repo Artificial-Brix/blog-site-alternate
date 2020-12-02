5e029ab07ded98374b5f1ae143e05438d97e825e
PratikDey <56970651+PratikDey@users.noreply.github.com>
2020-12-02 23:14:09 +0530

    added pagination and styling left to do

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import trending from "../assets/mocks/trending";
 import featured from "../assets/mocks/featured";
-import { MasonryPost, PostMasonry } from "../components/common";
+import { MasonryPost, PostMasonry, PostGrid } from "../components/common";
 
 const trendingConfig = {
   1: {
